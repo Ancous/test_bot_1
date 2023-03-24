fe6c43cabd694e6692022b5604786973687cc350
Alex <alex_taras@bk.ru>
2023-03-24 17:14:08 +0300

    commit_2

--- telegram_api/activate_bot/activate.py
+++ telegram_api/activate_bot/activate.py
@@ -1,5 +1,8 @@
 import telebot
+import os
+from dotenv import load_dotenv
 
 
-TOKEN_BOT = "6259686891:AAFQNLLJ_IEoT5s1jB8cTC4PPbAc1A6hCWY"
-bot = telebot.TeleBot(TOKEN_BOT)
+load_dotenv()
+
+bot = telebot.TeleBot(os.getenv("BOT_TOKEN"))
